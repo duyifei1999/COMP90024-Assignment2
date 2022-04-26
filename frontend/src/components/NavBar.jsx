76e83f39e6fcc037ff7cd4d00058c908fa0270ae
LinyanZ <linyanz1@student.unimelb.edu.au>
2022-04-27 00:23:09 +1000

    Add simple css to navigation bar.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,21 +1,12 @@
 import { NavLink } from "react-router-dom"
 
-// TODO: use style class
-let activeStyle = {
-    color: "red"
-}
-
 const NavBar = () => {
-    return (<>
+    return (
         <nav>
-            <NavLink style={({ isActive }) => 
-                isActive ? activeStyle : undefined
-            } to="/map">Map</NavLink> |{" "}
-            <NavLink style={({ isActive }) => 
-                isActive ? activeStyle : undefined
-            } to="/charts">Charts</NavLink>
+            <div><NavLink to="/map">Map</NavLink></div>
+            <div><NavLink to="/charts">Charts</NavLink></div>
         </nav>
-    </>);
+    );
 }
  
 export default NavBar;
