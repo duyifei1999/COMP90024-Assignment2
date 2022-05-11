d0dba1b444df4df55a9cf14c2dba40232c392de6
Linyan Zhu <linyanz1@student.unimelb.edu.au>
2022-05-11 20:25:50 +1000

    Change directory structures for docker-compose.

--- web/backend/server.js
+++ web/backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const db = require("nano")("http://admin:admin@localhost:5984");
+const db = require("nano")(
+  process.env.CHOUCHDB_HOST || "http://admin:admin@localhost:5984"
+);
 
 require("./startup/cors")(app);
 require("./startup/routes")(app, db);
 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
