f7ed3b481006acbba598daf747777f2eb42ba130
zhaopengl <zhaopengl@student.unimelb.edu.au>
2022-05-11 21:34:12 +1000

    json file with data having sa2 value

--- tweepy_language/couchdbp.py
+++ tweepy_language/couchdbp.py
@@ -4,16 +4,31 @@ from textblob import TextBlob
 from data_processing.spatial import SpatialTool
 tool = SpatialTool()
 tool.load_region_info("data_processing/sa2.json")
-db_tweet_name = 'tweets_housing'
-db_address = "http://127.0.0.1:5984/"
+db_tweet_name = 'language'
+db_address = "http://localhost:5984/"
 db_server = couchdb.Server(db_address)
 db_server.resource.credentials = ('admin', 'admin')
 tweets=[]
-file=open('tweets_housing.json','w')
+file=open('language.json','w')
 print(db_server)
 for name in db_server:
     print(name)
-
+f=open('db.json')
+file2=json.load(f)
+for x in file2['rows']:
+    data=x['doc']
+    del data['_id']
+    del data['_rev']
+    if data['geo']!=None and data['geo']!={}:
+        coord=[]
+        coord.append((data['geo']['geo']['bbox'][0]+data['geo']['geo']['bbox'][2])/2)
+        coord.append((data['geo']['geo']['bbox'][1]+data['geo']['geo']['bbox'][3])/2)
+        place=tool.locate(coords=coord)                    
+        if place !='outside melbourne':
+            data['sa2']=place
+            tweets.append(data)
+initial=json.dumps(tweets)
+file.write(initial)
 
 '''del db_server['tweets_housing']
 db_tweets=db_server['tweets_housing']
@@ -34,8 +49,8 @@ for x in db_server['tweets']:
                                 data_json['sentiment']=value
                                 db_tweets.save(data_json)
 '''
-for x in db_server['tweets_housing']:
+'''for x in db_server['tweets_housing']:
     tweets.append(db_server['tweets_housing'][x])
 initial=json.dumps(tweets)
-file.write(initial)    
+file.write(initial) '''   
     
