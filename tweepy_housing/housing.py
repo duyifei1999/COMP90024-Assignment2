431c2ac35e69f14b6eb4eedd4a2632c29d71cb1a
zhaopengl <zhaopengl@student.unimelb.edu.au>
2022-05-13 14:56:15 +1000

    update on data processing

--- tweepy_housing/housing.py
+++ tweepy_housing/housing.py
@@ -6,7 +6,7 @@ from textblob import TextBlob
 from data_processing.spatial import SpatialTool
 file = open('housing.txt', 'a')
 tool = SpatialTool()
-tool.load_region_info("data_processing/sa2.json")
+tool.load_region_info("data_processing/SA2_2016_MELB.json")
 ls=[]
 
 class TweetListener(StreamingClient):
