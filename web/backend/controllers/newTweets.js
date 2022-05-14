8dda4622d24845e3e0691470118d298231cbe263
Linyan Zhu <linyanz1@student.unimelb.edu.au>
2022-05-14 18:42:20 +1000

    Extend backend api.

--- web/backend/controllers/newTweets.js
+++ web/backend/controllers/newTweets.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const db = require("nano")(
   process.env.CHOUCHDB_HOST || "http://admin:admin@localhost:5984"
 );
-const oldTweetsDBName = process.env.OLD_TWEETS_DB_NAME || "old_tweets";
+const newTweetsDBName = process.env.NEW_TWEETS_DB_NAME || "tweets";
 
 const getHousing = async (req, res) => {
   try {
-    const tweets = await db.use(oldTweetsDBName);
+    const tweets = await db.use(newTweetsDBName);
     const result = await tweets.view("data_processing", "housing", {
       group_level: req.query.group_level || 1,
     });
@@ -18,7 +18,7 @@ const getHousing = async (req, res) => {
 
 const getLanguage = async (req, res) => {
   try {
-    const tweets = await db.use(oldTweetsDBName);
+    const tweets = await db.use(newTweetsDBName);
     const result = await tweets.view("data_processing", "language", {
       group_level: req.query.group_level || 1,
     });
--- web/backend/startup/routes.js
+++ web/backend/startup/routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const oldTweets = require("../routes/oldTweets.js");
+const newTweets = require("../routes/newTweets.js");
+
+const oldTweetsDBName = process.env.OLD_TWEETS_DB_NAME || "old_tweets";
+const newTweetsDBName = process.env.NEW_TWEETS_DB_NAME || "tweets";
 
 module.exports = function (app) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.use("/api/oldTweets", oldTweets);
+  app.use(`/api/${oldTweetsDBName}`, oldTweets);
+  app.use(`/api/${newTweetsDBName}`, newTweets);
 
   app.get("/", (req, res) => res.send("HOMEPAGE"));
   app.all("*", (req, res) => res.status(404).send("NOT FOUND"));
