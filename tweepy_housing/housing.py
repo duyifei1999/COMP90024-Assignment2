135500a65c5ac82afa80311d46ce7792ffb9bd23
RuiWangMSE <80255627+RuiWangMSE@users.noreply.github.com>
2022-05-15 12:49:42 +0800

    tweepy ip correction

--- tweepy_housing/housing.py
+++ tweepy_housing/housing.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
 
     db_tweet_name = 'housing'
-    db_address = "http://@172.26.129.154:5984/"
+    db_address = "http://172.26.129.154:5984/"
     db_server = couchdb.Server(db_address)
     db_server.resource.credentials = ('admin', 'admin')
     print(db_server)
